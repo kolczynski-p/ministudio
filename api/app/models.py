dbbe6b7b60c72e755fca976df905d8d20c1cbb16
Kolczyński <przekolczynski@gmail.com>
2021-03-04 06:08:34 +0100

    Changed models and endpoints to the destined

--- api/app/models.py
+++ api/app/models.py
@@ -1,12 +1,59 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
+from string import Template
 
 db = SQLAlchemy()
 
 
-class GalleryPhoto(db.Model):
-    __tablename__ = "gallery_photos"
-    id = db.Column(db.Integer, primary_key=True)
-    path = db.Column(db.String(120), nullable=False)
+class Work(db.Model):
+    __tablename__ = 'Works'
+    workId = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(512), nullable=False)
+    additionDate = db.Column(db.Date(), nullable=False)
+    photos = db.relationship('GalleryPhoto', backref='Works', lazy=True)
+
+    def __init__(self, title='example title', description='example description'):
+        self.title = title
+        self.description = description
+        date_stamp = date.today()
+        self.additionDate = date_stamp
 
     def __repr__(self):
-        return '<PHOTO: ID {},  PATH {}>'.format(self.id, self.path)
+        return '<WORK: ID {}, TITLE {}, DESCRIPTION {}, ADDITIONDATE {}>'.format(self.workId, self.title, self.description, self.additionDate)
+
+    def make_dict(self):
+        additionDate_string = self.additionDate.strftime('%d/%m/%Y')
+        dictionary = dict(workId=self.workId, title=self.title, description=self.description, additionDate=additionDate_string)
+        return dictionary
+
+
+class GalleryPhoto(db.Model):
+    __tablename__ = 'GalleryPhotos'
+    photoName = db.Column(db.String(255), primary_key=True)
+    galleryIndex = db.Column(db.Integer)
+    workId = db.Column(db.Integer, db.ForeignKey('Works.workId'), nullable=False)
+
+    def __init__(self, photoName, workId, galleryIndex=None):
+        self.photoName = photoName
+        self.workId = workId
+        self.galleryIndex = galleryIndex
+
+    def __repr__(self):
+        return '<PHOTO: NAME {}, WORKID {}, INDEX {}>'.format(self.photoName, self.workId, self.galleryIndex)
+
+    def make_dict(self):
+        dictionary = dict(photoName=self.photoName, galleryIndex=self.galleryIndex, workId=self.workId, )
+        return dictionary
+
+
+# example db populating for testing
+def TestPopulate():
+    work = Work('Człowiek-robot nr. jeden', 'Robotyczny cyborg czlowiek-maszyna')
+    db.session.add(work)
+    db.session.commit()
+
+    db.session.add_all([GalleryPhoto('work1_1.jpg', work.workId, 1), GalleryPhoto('work1_2.jpg', work.workId, 2), GalleryPhoto('work1_3.jpg', work.workId, 3), GalleryPhoto('work1_4.jpg', work.workId, 4)])
+    db.session.commit()
+
+#  from app.models import db, Work, GalleryPhoto, TestPopulate
--- api/app/routes.py
+++ api/app/routes.py
@@ -1,33 +1,56 @@
 from flask import Blueprint
 from flask import request
 import os
-from .models import GalleryPhoto, db
+from .models import GalleryPhoto, Work, db
 import json
 
 routes_bp = Blueprint('routes_bp', __name__)
 
 
-@routes_bp.route('/', methods=['GET'])
-def home_get():
-    gallery_photos_raw = [P.__repr__() for P in GalleryPhoto.query.all()]
-    photos_list_string = ' '.join(gallery_photos_raw)
+# Works requests
 
-    return photos_list_string
+@routes_bp.route('/works', methods=['GET'])
+def get_works():
+    result_json = '{works: ['
+    works = [json.dumps(W.make_dict()) for W in Work.query.all()]
+    result_json += ', '.join(works) + ']}'
 
-
-@routes_bp.route('/item/<photo_path>', methods=['GET'])
-def get_item(photo_path):
-    gallery_photo = GalleryPhoto.query.filter_by(path=photo_path).first()
-
-    return gallery_photo.__repr__()
+    return result_json
 
 
+@routes_bp.route('/works/<id>', methods=['GET'])
+def get_work(id):
+    work = Work.query.filter_by(workId=id).first()
+    result_json = json.dumps(work.make_dict())
+
+    return result_json
+
+
+# GalleryPhotos requests
+
+@routes_bp.route('/works/photos', methods=['GET'])
+def get_photos():
+    result_json = '{photos: ['
+    gallery_photos = [json.dumps(P.make_dict()) for P in GalleryPhoto.query.all()]
+    result_json += ', '.join(gallery_photos) + ']}'
+
+    return result_json
+
+
+@routes_bp.route('/works/photos/<index>', methods=['GET'])
+def get_photo(index):
+    gallery_photo = GalleryPhoto.query.filter_by(galleryIndex=index).first()
+    result_json = json.dumps(gallery_photo.make_dict())
+
+    return result_json
 
 
 @routes_bp.route('/post', methods=['POST'])
 def home_post():
     request_body = request.json
-    photo = GalleryPhoto(path=request_body['path']) 
+    photo = GalleryPhoto(path=request_body['path'])
     db.session.add(photo)
     db.session.commit()
     return 'Ok'
+
+
